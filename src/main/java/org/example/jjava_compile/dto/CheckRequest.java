050895827bcbaf4ce5e89fc000e7f6bcccb66a4f
parangdajavous <nuit0535@gmail.com>
2025-08-11 14:42:00 +0900

    문제ã검산 컴파일 완료

--- src/main/java/org/example/jjava_compile/dto/CheckRequest.java
+++ src/main/java/org/example/jjava_compile/dto/CheckRequest.java
@@ -1,4 +1,36 @@
 package org.example.jjava_compile.dto;
 
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
 public class CheckRequest {
+
+    @Data
+    public static class DTO {
+        private String type;
+        private String payload;
+        private List<TestSpecDTO> tests;
+
+        @Data
+        public static class TestSpecDTO {
+            private String prelude;
+            private Map<String, Object> testVariable;
+            private String testAnswer;
+
+            public TestSpecDTO(String prelude, Map<String, Object> testVariable, String testAnswer) {
+                this.prelude = prelude;
+                this.testVariable = testVariable;
+                this.testAnswer = testAnswer;
+            }
+        }
+
+
+        public DTO(String type, String payload, List<TestSpecDTO> tests) {
+            this.type = type;
+            this.payload = payload;
+            this.tests = tests;
+        }
+    }
 }
--- src/main/java/org/example/jjava_compile/dto/CheckResponse.java
+++ src/main/java/org/example/jjava_compile/dto/CheckResponse.java
@@ -1,4 +1,43 @@
 package org.example.jjava_compile.dto;
 
+import lombok.Data;
+
+import java.util.Map;
+
 public class CheckResponse {
+
+    @Data
+    public static class DTO {
+        private boolean passed; // 성공, 실패 여부
+        private String message;      // "정답입니다." / "오답입니다."
+        private String result;          // 실제 출력
+        private String code;  // 사용자 코드
+
+        public DTO(boolean passed, String message, String result, String code) {
+            this.passed = passed;
+            this.message = message;
+            this.result = result;
+            this.code = code;
+        }
+    }
+
+    @Data
+    public static class FailDTO {
+        private boolean passed; // 성공, 실패 여부
+        private Integer index;                  // 테스트 인덱스(0부터)
+        private Map<String, Object> failedInputs;  // bindings 원본
+        private String message;      // "정답입니다." / "오답입니다."
+        private String expected;            // 기대값
+        private String result;              // 실제 출력
+
+
+        public FailDTO(boolean passed, Integer index, Map<String, Object> failedInputs, String message, String expected, String result) {
+            this.passed = passed;
+            this.index = index;
+            this.failedInputs = failedInputs;
+            this.message = message;
+            this.expected = expected;
+            this.result = result;
+        }
+    }
 }
